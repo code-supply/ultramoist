167dffbb56c8a4e9a7b422bd70ceadd2bef220af
Andrew Bruce <me@andrewbruce.net>
2026-08-06 10:54:43 +0100

    Add secp256k1 point addition and doubling

--- lib/ultramoist/secp256k1.ex
+++ lib/ultramoist/secp256k1.ex
@@ -23,4 +23,25 @@ defmodule Ultramoist.Secp256k1 do
   def mod_sqrt(a) do
     :crypto.mod_pow(a, div(@field_prime + 1, 4), @field_prime) |> :binary.decode_unsigned()
   end
+
+  def point_add({x1, y1}, {x1, y1}) do
+    slope = Integer.mod(3 * x1 * x1 * mod_inv(2 * y1), @field_prime)
+    x3 = Integer.mod(slope * slope - 2 * x1, @field_prime)
+    y3 = Integer.mod(slope * (x1 - x3) - y1, @field_prime)
+    {x3, y3}
+  end
+
+  def point_add({x1, y1}, {x2, y2}) do
+    slope = Integer.mod((y2 - y1) * mod_inv(x2 - x1), @field_prime)
+    x3 = Integer.mod(slope * slope - x1 - x2, @field_prime)
+    y3 = Integer.mod(slope * (x1 - x3) - y1, @field_prime)
+    {x3, y3}
+  end
+
+  defp mod_inv(a) do
+    a
+    |> Integer.mod(@field_prime)
+    |> :crypto.mod_pow(@field_prime - 2, @field_prime)
+    |> :binary.decode_unsigned()
+  end
 end
--- test/ultramoist/secp256k1_test.exs
+++ test/ultramoist/secp256k1_test.exs
@@ -2,10 +2,16 @@ defmodule Ultramoist.Secp256k1Test do
   use ExUnit.Case, async: true
 
   @curve_order 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
+  @generator_x 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798
   @generator_y 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
   # The generator point's y^2 mod p (i.e. Gx^3 + 7 mod p), independently
   # precomputed rather than derived by the formula under test.
   @generator_y_squared 0x4866D6A5AB41AB2C6BCC57CCD3735DA5F16F80A548E5E20A44E4E9B8118C26F2
+
+  @double_generator_x 0xC6047F9441ED7D6D3045406E95C07CD85C778E4B8CEF3CA7ABAC09B95C709EE5
+  @double_generator_y 0x1AE168FEA63DC339A3C58419466CEAEEF7F632653266D0E1236431A950CFE52A
+  @triple_generator_x 0xF9308A019258C31049344F85F89D5229B531C845836F99B08601F113BCE036F9
+  @triple_generator_y 0x388F7B0F632DE8140FE337E62A37F3566500A99934C2231B6CB9FD7584B8E672
 
   @priv_key :crypto.hash(:sha256, "private key")
   @digest Ultramoist.Keccak.hash256("test")
@@ -41,5 +47,22 @@ defmodule Ultramoist.Secp256k1Test do
   test "computes the modular square root of a quadratic residue" do
     assert Ultramoist.Secp256k1.mod_sqrt(4) == 2
     assert Ultramoist.Secp256k1.mod_sqrt(@generator_y_squared) == @generator_y
+  end
+
+  # @spec SIGN-DATA-003b
+  test "adds two distinct points on the curve" do
+    g = {@generator_x, @generator_y}
+    two_g = {@double_generator_x, @double_generator_y}
+    three_g = {@triple_generator_x, @triple_generator_y}
+
+    assert Ultramoist.Secp256k1.point_add(g, two_g) == three_g
+  end
+
+  # @spec SIGN-DATA-003b
+  test "doubles a point (adds a point to itself)" do
+    g = {@generator_x, @generator_y}
+    two_g = {@double_generator_x, @double_generator_y}
+
+    assert Ultramoist.Secp256k1.point_add(g, g) == two_g
   end
 end
