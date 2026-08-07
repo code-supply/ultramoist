2abf5972294acd0ef089e133bfc501644684bb1f
Andrew Bruce <me@andrewbruce.net>
2026-08-07 18:03:35 +0100

    Add subscription request builders for userFills, clearinghouseState, userNonFundingLedgerUpdates

---
+++ lib/ultramoist/subscription.ex
@@ -0,0 +1,25 @@
+defmodule Ultramoist.Subscription do
+  @moduledoc false
+
+  # @spec SUB-DATA-001
+  def user_fills(user) do
+    build("userFills", [{"user", user}])
+  end
+
+  def main_dex, do: ""
+
+  # @spec SUB-DATA-002
+  def clearinghouse_state(user, dex) do
+    build("clearinghouseState", [{"user", user}, {"dex", dex}])
+  end
+
+  # @spec SUB-DATA-003
+  def user_non_funding_ledger_updates(user) do
+    build("userNonFundingLedgerUpdates", [{"user", user}])
+  end
+
+  defp build(type, params) do
+    key = Enum.join([type | Enum.map(params, fn {_field, value} -> value end)], ":")
+    {key, Map.new([{"type", type} | params])}
+  end
+end
---
+++ test/ultramoist/subscription_test.exs
@@ -0,0 +1,25 @@
+defmodule Ultramoist.SubscriptionTest do
+  use ExUnit.Case, async: true
+
+  # @spec SUB-DATA-001
+  test "builds a userFills subscription request and dedup key for a user" do
+    assert Ultramoist.Subscription.user_fills("0xabc") ==
+             {"userFills:0xabc", %{"type" => "userFills", "user" => "0xabc"}}
+  end
+
+  # @spec SUB-DATA-002
+  test "builds a clearinghouseState subscription request and dedup key for a user and dex" do
+    dex = Ultramoist.Subscription.main_dex()
+
+    assert Ultramoist.Subscription.clearinghouse_state("0xabc", dex) ==
+             {"clearinghouseState:0xabc:",
+              %{"type" => "clearinghouseState", "user" => "0xabc", "dex" => dex}}
+  end
+
+  # @spec SUB-DATA-003
+  test "builds a userNonFundingLedgerUpdates subscription request and dedup key for a user" do
+    assert Ultramoist.Subscription.user_non_funding_ledger_updates("0xabc") ==
+             {"userNonFundingLedgerUpdates:0xabc",
+              %{"type" => "userNonFundingLedgerUpdates", "user" => "0xabc"}}
+  end
+end
